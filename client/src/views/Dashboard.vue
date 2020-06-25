600594198929b8139daf04d2557d2111a43044d6
Cokro Siamdani <cokrosiamdani@gmail.com>
2020-06-25 21:20:58 +0700

    baru nickname dan dashboard

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </div>
+    ini gambar apa hayooo????
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  created() {
+    if (localStorage.nick_name) {
+      this.$router.push("/dashboard", () => {});
+    } else {
+      this.$router.push("/", () => {});
+    }
+  }
+};
+</script>
+
+<style></style>
