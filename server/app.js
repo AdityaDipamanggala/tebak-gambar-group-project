d65777d232ac33c2d4c76ed78cfaba736ac58cdb
Beni Adi Pranata <beniadipranata@gmail.com>
2020-06-25 17:38:35 +0700

    Server done

--- server/app.js
+++ server/app.js
@@ -1,15 +1,31 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+const db = require('./db.json');
+
+const getRandom = () => {
+    return db[Math.floor(Math.random() * db.length)];
+};
+
+let current = getRandom();
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    io.emit('question', current.image);
+
+    socket.on('answer', (data) => {
+        data.answer = data.answer.toString();
+        if ('true' === data.answer.trim().toLowerCase()) {
+            current = getRandom();
+            io.emit('question', current.image);
+            io.emit(data.id, true);
+        } else {
+            io.emit(data.id, false);
+        }
+    });
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
