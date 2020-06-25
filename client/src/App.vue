00c5ea6326d6950f67a854401fe2982ee1ebdf23
unknown <rahmatgilangmaulana9@gmail.com>
2020-06-25 21:32:32 +0700

    integrasi socket client ke store

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,17 +2,30 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ image }}
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import store from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  created() {
+        socket.on('question', (image) => {
+            console.log(image);
+            this.image = image;
+        });
+        socket.on(this.id, (isCorrect) => {
+            if (isCorrect) this.point += 10;
+            else this.point--;
+        });
+    },
 }
 </script>
 
