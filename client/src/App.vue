9d214e92f402db0353bac1cbd6ab30fc028412f5
Beni Adi Pranata <beniadipranata@gmail.com>
2020-06-26 14:21:07 +0700

    Separate navbar into component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <Navbar></Navbar>
         <!-- <router-link to="/">XXX</router-link> -->
         <transition name="fade">
             <router-view />
@@ -8,8 +9,10 @@
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
 export default {
     name: 'App',
+    components: { Navbar },
     mounted() {
         const sound = new Audio('musicSaja.mp3');
         sound.volume = 0.5;
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,40 +1,6 @@
 <template>
     <div>
         <div class="container">
-            <nav class="navbar navbar-light bg-transparent justify-content-between fixed-top">
-                <!-- <div class="container ml-5 mr-5"> -->
-                <a class="navbar-brand ml-5" style="color: brown;"
-                    ><strong>Guess The Word Game</strong></a
-                >
-                <div class="dropdown posisi">
-                    <button
-                        class="btn btn-primary dropdown-toggle"
-                        type="button"
-                        data-toggle="dropdown"
-                    >
-                        Leaderboard <span class="caret"></span>
-                    </button>
-
-                    <table id="dropdown" class="table dropdown-menu text-center table-borderless">
-                        <tbody>
-                            <tr v-for="(data, i) in this.leaderData" :key="i" class="p-0 m-0">
-                                <th class="text-left">{{ data.nick }}</th>
-                                <th class="text-center">{{ data.point }} pts</th>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <button
-                    class="btn btn-danger my-0 my-sm-0 mr-5"
-                    type="button"
-                    @click.prevent="logout"
-                >
-                    Logout
-                </button>
-                <!-- </div> -->
-            </nav>
-        </div>
-        <div class="container">
             <h1 class="marginatas">Let's guess the picture !</h1>
             <h1>Point: {{ point }}</h1>
 
@@ -136,7 +102,8 @@ export default {
         }, 3000);
 
         socket.on('getLeaderboard', data => {
-            this.leaderData = data;
+            console.log(data);
+            this.$store.commit('setLeaderData', data);
         });
     }
 };
@@ -192,11 +159,4 @@ form {
     width: 45%;
 }
 
-table {
-    width: 250px;
-}
-
-.posisi {
-    margin-right: 15vh;
-}
 </style>
